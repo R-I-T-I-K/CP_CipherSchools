3dbd3cbf73fb1d3763c29e2056679b19b452ca49
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-14 23:02:36 +0530

    Done

--- Day 1/Assignments/B. Find Majority Element (Medium)/MajorEle.cpp
+++ Day 1/Assignments/B. Find Majority Element (Medium)/MajorEle.cpp
@@ -1,1 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void majorityElement(vector<int> arr)
+{
+
+   int maj=0;//store majority count
+   int maj_ind=-1;//store majority index
+   int l=arr.size();
+
+   //looping and counting the elements one by one
+   for(int i=0;i<l;i++)
+   {
+      int count=0;
+      for(int j=0;j<l;j++)
+      {
+         if(arr[i]==arr[j])
+         {
+            count++;
+         }
+         if(count>maj)
+         {
+            maj=count;
+            maj_ind=i;
+         }
+      }
+   }
+   //checking if any element satisfy majority condition
+   if(maj>l/2)
+   {
+      cout<<arr[maj_ind];
+   }
+   else{cout<<"No Major Element";}
+}
+
+//main function
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   vector<int> arr = {1, 1, 1, 2, 1, 1, 2, 2, 2};
+   majorityElement(arr);
+
+   return 0;
+}
