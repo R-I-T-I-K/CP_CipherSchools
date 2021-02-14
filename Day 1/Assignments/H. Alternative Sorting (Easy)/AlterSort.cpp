ef3a663cefc2d4a7a29b2d8eee91eb3a394647ed
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-14 14:41:15 +0530

    Done

--- Day 1/Assignments/H. Alternative Sorting (Easy)/AlterSort.cpp
+++ Day 1/Assignments/H. Alternative Sorting (Easy)/AlterSort.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   vector<int> arr={1,9,2,8,3,7,4,6,5,10};
+
+   sort(arr.begin(),arr.end(),greater<int>());
+   //just printing one maximum and one minimum element 
+   int i=0;
+   int j=arr.size()-1;
+   while(i<j)
+   {
+      cout<<arr[i]<<" "<<arr[j]<<" ";
+      i++;
+      j--;
+   }
+  //if arr have odd no.of elements
+   if (arr.size() % 2 != 0) 
+        cout << arr[i];
+   return 0;
+}
