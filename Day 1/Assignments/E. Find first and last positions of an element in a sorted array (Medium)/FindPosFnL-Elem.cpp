31ab5cc406c1e5bfdf6abb3723fa2adcbd2e7a5b
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-15 01:27:08 +0530

    Done

--- Day 1/Assignments/E. Find first and last positions of an element in a sorted array (Medium)/FindPosFnL-Elem.cpp
+++ Day 1/Assignments/E. Find first and last positions of an element in a sorted array (Medium)/FindPosFnL-Elem.cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void PosFnL(vector<int> vec,int ele)
+{
+   int l=vec.size();//length of vector
+   int i;           //iterator
+   int j;           //iterator
+   int F=0;         //to save 1st occurence
+   int L=0;         //to save last occurence
+
+   //fiding the position
+   for( i=0,j=l-1; i<l,j>=0; i++,j--)
+   {
+      if(vec[i]==ele)
+      {
+         L=i;
+      }
+      if(vec[j]==ele)
+      {
+         F=j;
+      }
+   }
+   cout<<F<<","<<L;
+}
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   vector<int> vec = { 1, 2, 4, 5, 7, 4, 9};
+
+   PosFnL(vec,7);
+
+   return 0;
+}
