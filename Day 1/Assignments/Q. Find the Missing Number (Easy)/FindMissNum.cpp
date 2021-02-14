19baba3681aa4ce394734ab5df204665037a73f6
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-14 16:11:21 +0530

    Done

--- Day 1/Assignments/Q. Find the Missing Number (Easy)/FindMissNum.cpp
+++ Day 1/Assignments/Q. Find the Missing Number (Easy)/FindMissNum.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+   vector<int> arr={1,2,3,4,6,7,8,9};
+  
+    //Find sum of elements
+   int sum=accumulate(arr.begin(),arr.end(),0);
+   int l=arr.size();
+   
+   //Find sum of element of a normal vector of length N.
+   vector<int> arr1={};
+   for(int i=0;i<=l;i++)
+   {
+      arr1.push_back(i+1);
+   }
+
+   int sum1=accumulate(arr1.begin(),arr1.end(),0);
+   cout<<sum1-sum;
+}
