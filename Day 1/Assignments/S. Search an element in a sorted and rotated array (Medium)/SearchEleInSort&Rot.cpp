21a36f65221690254ac252274ba7b1991013a6b8
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-15 23:14:33 +0530

    Done

--- Day 1/Assignments/S. Search an element in a sorted and rotated array (Medium)/SearchEleInSort&Rot.cpp
+++ Day 1/Assignments/S. Search an element in a sorted and rotated array (Medium)/SearchEleInSort&Rot.cpp
@@ -1,1 +1,41 @@
+#include <bits/stdc++.h> 
+using namespace std; 
 
+
+//Just like binary search
+int SortAndRot(vector<int> vec, int l, int h, int key) 
+{ 
+	if (l > h) 
+		return -1; 
+
+	int mid = (l + h) / 2; 
+	if (vec[mid] == key) 
+		return mid; 
+
+	if (vec[l] <= vec[mid]) 
+   { 
+      //if above condition is true mean subarray is sorted
+		if (key >= vec[l] && key <= vec[mid]) 
+			return SortAndRot(vec, l, mid - 1, key); 
+		
+		return SortAndRot(vec, mid + 1, h, key); 
+	} 
+   //as 1st subarray is not sorted then 2nd subarray must be sorted
+	if (key >= vec[mid] && key <= vec[h]) 
+		return SortAndRot(vec, mid + 1, h, key); 
+
+	return SortAndRot(vec, l, mid - 1, key); 
+} 
+
+int main() 
+{ 
+	vector<int> vec = { 4, 5, 6, 7, 8, 9, 1, 2, 3 }; 
+	int n = vec.size(); 
+	int x = 1; 
+	int i = SortAndRot(vec, 0, n - 1, x); 
+
+	if (i != -1) 
+		cout << "The element is at index: " << i << endl; 
+	else
+		cout << "Element not found"; 
+} 
