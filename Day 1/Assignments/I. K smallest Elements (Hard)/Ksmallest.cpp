e05e0f1a6a06215de97c318c3b528d1a6afeb0c2
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-15 23:32:31 +0530

    Done

--- Day 1/Assignments/I. K smallest Elements (Hard)/Ksmallest.cpp
+++ Day 1/Assignments/I. K smallest Elements (Hard)/Ksmallest.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int kthSmallest(vector<int> vec, int n, int k)
+{
+   //sorting the array
+	sort(vec.begin(),vec.end());
+
+   //returning the kth element
+	return vec[k - 1];
+}
+
+int main()
+{
+	vector<int> vec = { 12, 3, 5, 7, 19 };
+	int k=3;
+   int n=vec.size();
+	cout << "K'th smallest element is " << kthSmallest(vec, n, k);
+	return 0;
+}
