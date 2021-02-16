c873ef9be93e42cd528d98e6cda7aa7b83035c88
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-16 23:28:20 +0530

    Done

--- Day 2/Assignment/Fib&Fac-TailRecur.cpp
+++ Day 2/Assignment/Fib&Fac-TailRecur.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+//Factorial using recursion
+int factorial(int n)
+{
+   if(n==1)
+   {
+      return 1;
+   }
+   else
+   return n*factorial(n-1);
+}
+
+//Fibonacci upto n
+int Fibo(int n)
+{
+   if(n<=1)
+   {
+      return n;
+   }
+   else
+      return Fibo(n-1)+Fibo(n-2);
+}
+
+
+int main()
+{
+   int n=7;
+   cout<<"Factorial is: "<<factorial(n)<<endl;
+   cout<<"Nth Fibonacci number: "<<Fibo(n);
+
+}
