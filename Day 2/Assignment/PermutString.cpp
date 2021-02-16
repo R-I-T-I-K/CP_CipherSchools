2efdac94fe1215fb819115776cd8dd4f5e2af507
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-17 01:42:02 +0530

    Done

--- Day 2/Assignment/PermutString.cpp
+++ Day 2/Assignment/PermutString.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void generatePermutationHelper(string &str, int start, int end, vector<string>& result) 
+{
+    if (start >= end) 
+    {
+        result.push_back(str);
+    } 
+    else 
+    {
+        for (int i = start; i <= end; i++) 
+        {
+            swap(str[i], str[start]);
+            generatePermutationHelper(str, start+1, end, result);
+            //backtracking
+            swap(str[i], str[start]);
+        }
+    }
+}
+
+vector<string> generatePermutation(string str) 
+{
+    vector<string> result;
+    generatePermutationHelper(str, 0, str.size() - 1, result); 
+    return result;
+}
+
+int main() 
+{
+    vector<string> result = generatePermutation("XYZ");
+    for (string str : result) 
+    {
+        cout << str << " ";
+    }
+}
