b3d7020152eea095931b3ecc46e6aac4595dbc1f
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-17 01:34:40 +0530

    Done

--- Day 2/Assignment/PossiblePaths.cpp
+++ Day 2/Assignment/PossiblePaths.cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+//Valid Path or Not
+bool isValid(int x,int y,int a,int b){
+    if(a<=x && b<=y){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//Counting Paths
+void totalPaths(int row,int column,int curr_row,int curr_column,int &count){
+    if(!isValid(row,column,curr_row,curr_column)){
+        return;
+    }
+    if(row==curr_row && column==curr_column){
+        count++;
+    }
+
+    totalPaths(row,column,curr_row+1,curr_column,count);
+    totalPaths(row,column,curr_row,curr_column+1,count);
+    
+
+}
+
+int main()
+{
+
+    int row=3,column=3;
+    int res=0;
+    totalPaths(row,column,1,1,res);
+    cout<<res<<endl;   
+    
+    return 0;
+}
