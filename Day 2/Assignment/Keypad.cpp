ddd63960a91f677340c0ad465301d0415ca09309
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-17 01:27:22 +0530

    Done

--- Day 2/Assignment/Keypad.cpp
+++ Day 2/Assignment/Keypad.cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+vector <string> keyPad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+void PadHelper(vector<int> &number,vector<string> &res,int size,int curr_size,string curr){
+    if(curr_size==size){
+        res.push_back(curr);
+        return;
+    }
+
+    string possibleNum = keyPad[number[curr_size]];
+    for (int i = 0; i < possibleNum.length(); i++)
+    {
+        PadHelper(number,res,size,curr_size+1,curr+possibleNum[i]);
+    }
+    
+}   
+
+vector <string> phonePad(vector <int> &number,vector<string> &res){
+    int size=number.size();
+    PadHelper(number,res,size,0,"");
+    return res;
+}
+
+int main()
+{
+    vector <int> number={2,3,4};
+    vector <string> res;
+    phonePad(number,res);
+
+    for(string xx:res)
+    {
+       cout<<xx<<" ";
+    }
+    
+    return 0;
+}
