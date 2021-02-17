85a5410eaac4c6e7eb38cbc4229fecfe245597a1
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-17 22:54:35 +0530

    Done

--- Day 3/Assignments/Palindrome.cpp
+++ Day 3/Assignments/Palindrome.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        Node(0);
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+bool isPalindromeHelper(Node ** left, Node * right) {
+    // Has the rigght pointer reached the end of LL or not?
+    if (right == NULL) {
+        return true;
+    }
+    bool isPalin = isPalindromeHelper(left, right->next);
+    if (!isPalin) {
+        return false;
+    }
+
+    bool dataEqual = ((*left)->data == right->data);
+    (*left) = (*left)->next;
+    return dataEqual;
+}
+
+bool isPalindrome(Node * head){
+    return isPalindromeHelper(&head, head);
+}
+
+int main()
+{
+  
+}
