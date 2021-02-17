7c8b69fc02dad02f09bcce5a734e4f2e5c93f874
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-17 15:22:43 +0530

    Done

--- Day 2/Assignment/PowerSubset.cpp
+++ Day 2/Assignment/PowerSubset.cpp
@@ -1,1 +1,26 @@
+#include <iostream> 
+#include <math.h> 
+using namespace std; 
 
+void PowerSet(char *set, int set_size) 
+{ 
+	unsigned int pow_set_size = pow(2, set_size); 
+	int counter, j; 
+  
+	for(counter = 0; counter < pow_set_size; counter++) 
+	{ 
+	for(j = 0; j < set_size; j++) 
+	{ 
+		if(counter & (1 << j)) 
+			cout << set[j]; 
+	} 
+	cout << endl; 
+	} 
+} 
+
+int main() 
+{ 
+	char set[] = {'a','b','c'}; 
+	PowerSet(set, 3); 
+	return 0; 
+} 
