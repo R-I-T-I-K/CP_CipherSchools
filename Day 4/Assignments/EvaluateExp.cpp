88158fb668927eb0092f5f787e5eedbae0341ed7
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-18 19:53:29 +0530

    Done

---
+++ Day 4/Assignments/EvaluateExp.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int evaluateExpression(vector<string> &inputs)
+{
+    stack<int> S;
+    for (int i = 0; i < inputs.size(); i++)
+    {
+        if (inputs[i] == "+" || inputs[i] == "-" || inputs[i] == "*" || inputs[i] == "/")
+        {
+            int v1 = S.top(); S.pop();
+            int v2 = S.top(); S.pop();
+            if (inputs[i] == "+")
+                S.push(v2 + v1);
+            else if (inputs[i] == "-")
+                S.push(v2 - v1);
+            else if (inputs[i] == "*")
+                S.push(v2 * v1);
+            else if (inputs[i] == "/")
+                S.push(v2 / v1);
+        }
+        else
+            S.push(stoi(inputs[i]));
+    }
+    return S.top();
+}
+int main()
+{
+  vector<string> inputs = "53+45*";
+  cout<<evaluateExpression(inputs);
+}
