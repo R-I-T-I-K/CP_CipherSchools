de0fc708c65b22d2d332886dc3c6480a13866a92
Ritik Singh <47116757+R-I-T-I-K@users.noreply.github.com>
2021-02-18 19:39:37 +0530

    Done

--- Day 3/Assignments/Sort.cpp
+++ Day 3/Assignments/Sort.cpp
@@ -1,1 +1,66 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
 
+struct Node{
+    int data;
+    Node* next;
+    Node(){
+        Node(0);
+    }
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void addAtEnd(Node **head,int x){
+    Node *curr=*head;
+    Node *temp=new Node(x);
+    if(curr==NULL){
+        *head=temp;
+    }
+    else{
+        while(curr->next!=NULL){
+            curr=curr->next;
+        }
+        curr->next=temp;
+    }
+}
+void addAtFront(Node **head,int x){
+    Node *temp=new Node(x);
+    if(*head==NULL){
+        *head=temp;
+    }
+    else{
+        temp->next=*head;
+        *head=temp;
+    }
+
+}
+
+void printAll(Node **head){
+    Node *curr=*head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+bool idEmpty(Node **head){
+    if(*head==NULL){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
